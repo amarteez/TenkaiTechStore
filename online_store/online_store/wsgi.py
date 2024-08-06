dc8272a2689d27c5c1f395199b84b15a1dc06fee
Anel Martéz <anelmartez18@gmail.com>
2024-08-05 19:29:14 -0500

    Configuración de despliegue en Heroku

--- online_store/online_store/wsgi.py
+++ online_store/online_store/wsgi.py
@@ -1,7 +1,5 @@
-#wsgi.py de online_store
 import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_store.settings')
-
 application = get_wsgi_application()
