b1590abcd72b6cc4b743a523ac09c5128fdf489d
Anel Martéz <anelmartez18@gmail.com>
2024-08-06 01:15:44 -0500

    intentare el deploy con render

--- online_store/online_store/settings.py
+++ online_store/online_store/settings.py
@@ -1,32 +1,21 @@
 import os
 from pathlib import Path
 import environ
-import django_heroku
 
-# Initialize environment variables
 env = environ.Env(
     DEBUG=(bool, False)
 )
 
-# Base directory of the project
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Reading .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-# Secret key management
 SECRET_KEY = env('SECRET_KEY')
-
-# Debug mode management
 DEBUG = env('DEBUG')
+ALLOWED_HOSTS = ['your-render-subdomain.onrender.com', 'localhost', '127.0.0.1']
 
-# Allowed hosts management
-ALLOWED_HOSTS = ['tenkaitechstore.herokuapp.com', 'localhost', '127.0.0.1']
-
-# WSGI application path
 WSGI_APPLICATION = 'online_store.wsgi.application'
 
-# Database configuration
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -41,21 +30,18 @@ DATABASES = {
     }
 }
 
-# Installed applications
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'corsheaders',
     'store',
     'rest_framework',
 ]
 
-# Middleware configuration
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -68,10 +54,8 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-# Root URL configuration
 ROOT_URLCONF = 'online_store.urls'
 
-# Template settings
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -88,13 +72,11 @@ TEMPLATES = [
     },
 ]
 
-# CORS configuration
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://192.168.1.13:3000",
 ]
 
-# Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -110,23 +92,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Localization settings
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# Media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Static files configuration
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Activate Django-Heroku
-django_heroku.settings(locals())
